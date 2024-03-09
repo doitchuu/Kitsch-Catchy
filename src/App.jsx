42be1f408e082234d445a0fec59e17c6e0f254f7
seulgi chu <seulgichu14@gmail.com>
2024-03-09 14:47:05 +0900

    Fix: BrowserRouter 설정

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return <h1 className="text-xl text-rose-400">Kitsch-Catchy</h1>;
-}
-
-export default App;
---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import GlobalStyle from "./GlobalStyle";
+import Onboarding from "./Onboarding";
+import NewFilter from "./NewFilter";
+import CameraCapture from "./CameraCapture";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" exact element={<Onboarding />} />
+        <Route path="/new" element={<NewFilter />} />
+        <Route path="/camera" element={<CameraCapture />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
