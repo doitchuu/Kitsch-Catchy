f69d8c37882109e5eb344d679297cdf2e11157e2
seulgi chu <seulgichu14@gmail.com>
2024-03-13 18:29:58 +0900

    Fix: 캔버스 견본 이미지 및 이미지 추가 로직 구현

--- src/components/NewFilter/index.jsx
+++ src/components/NewFilter/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "@emotion/styled";
 
 import Sidebar from "../Sidebar";
@@ -12,11 +12,54 @@ function NewFilter() {
   const [isLoading, setIsLoading] = useState(false);
   const [isPopupOpened, setIsPopupOpened] = useState(true);
 
+  const canvasRef = useRef(null);
+
+  function renderCanvas() {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const baseImage = new Image();
+
+    baseImage.src = "src/assets/face_sample_image.png";
+    baseImage.onload = () => {
+      const scale = Math.min(
+        canvas.width / baseImage.width,
+        canvas.height / baseImage.height,
+      );
+      const x = canvas.width / 2 - (baseImage.width / 2) * scale;
+      const y = canvas.height / 2 - (baseImage.height / 2) * scale;
+      const width = baseImage.width * scale;
+      const height = baseImage.height * scale;
+
+      ctx.drawImage(baseImage, x, y, width, height);
+    };
+  }
+
   function handleClosePopup(event) {
     event.preventDefault();
 
     setIsPopupOpened(false);
   }
+
+  function addImageToCanvas(sticker) {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+
+    image.src = sticker.src;
+    image.onload = () => {
+      ctx.drawImage(
+        image,
+        (canvas.width - sticker.width) / 2,
+        (canvas.height - sticker.height) / 2,
+        sticker.width,
+        sticker.height,
+      );
+    };
+  }
+
+  useEffect(() => {
+    renderCanvas();
+  }, []);
 
   return (
     <>
@@ -29,10 +72,12 @@ function NewFilter() {
         </Modal>
       )}
       <FilterWrapper>
-        <Sidebar />
+        <Sidebar onImageClick={addImageToCanvas} />
         <FilterContainer>
           <Header />
-          <canvas />
+          <CanvasContainer>
+            <StyledCanvas ref={canvasRef} width="700" height="700" />
+          </CanvasContainer>
           {isPopupOpened && (
             <PopupContainer>
               Style Your Face Just Right! The better the fit, the cooler the
@@ -58,21 +103,37 @@ const FilterWrapper = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
+
+  background-color: #636366;
 `;
 
 const FilterContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  height: 100%;
+`;
+
+const CanvasContainer = styled.div`
+  padding: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledCanvas = styled.canvas`
+  background-color: #ffffff;
 `;
 
 const PopupContainer = styled.div`
-  position: relative;
+  position: fixed;
   z-index: 1;
-  bottom: 40px;
+  bottom: 20px;
+  right: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 380px;
   margin: 28px;
   padding: 24px;
   border-radius: 8px;
@@ -89,6 +150,7 @@ const PopupContainer = styled.div`
   .icon-close {
     margin-left: 16px;
     width: 24px;
+    opacity: 0.4;
   }
 `;
 
