5fd3f9c3afcf0b24b7cb23c25c184e589e26b768
seulgi chu <seulgichu14@gmail.com>
2024-04-08 15:53:51 +0900

    Refactor: 모바일 웹 접근 제한 로직 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import styled from "@emotion/styled";
 
 import GlobalStyle from "./GlobalStyle";
 import Onboarding from "./Onboarding";
@@ -7,6 +8,17 @@ import NewFilter from "./NewFilter";
 import CameraCapture from "./CameraCapture";
 
 function App() {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    return (
+      <MobileWrapper>
+        The kitsch catchy service is <br />
+        only available on PC web.😥
+      </MobileWrapper>
+    );
+  }
+
   return (
     <>
       <GlobalStyle />
@@ -18,5 +30,18 @@ function App() {
     </>
   );
 }
+
+const MobileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+
+  color: #111113;
+  font-size: 1rem;
+  font-weight: 700;
+`;
 
 export default App;
