a1756f3aafbb2bbfaf1b656664a11b53aa8e2328
doitchuu <seulgichu14@gmail.com>
2025-11-08 17:46:53 +0900

    Add: 얼굴 필터 관련 상수 파일 추가

---
+++ src/constants/faceFilter.js
@@ -0,0 +1,10 @@
+/**
+ * 얼굴 필터 관련 상수
+ */
+
+// 샘플 이미지 크기 (face_sample_image.png)
+export const SAMPLE_IMAGE_WIDTH = 1024;
+export const SAMPLE_IMAGE_HEIGHT = 1024;
+
+// 필터 생성 영역 크기
+export const FILTER_AREA_SIZE = 800;
